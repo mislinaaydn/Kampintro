20281f623ae55e0db7fbea09a95d0190553c9fa6
misgibiyazilim <75722301+mislinaaydn@users.noreply.github.com>
2021-01-21 01:13:50 +0300

    Update Program.cs

--- DegerVeReferansTipler1/Program.cs
+++ DegerVeReferansTipler1/Program.cs
@@ -8,14 +8,14 @@ namespace DegerVeReferansTipler
         {
             Console.WriteLine("Hello World!");
 
-            int sayi1 = 10;
-            int sayi2 = 30;
+            int sayi1 = 20;
+            int sayi2 = 50;
             sayi1 = sayi2;
-            sayi2 = 65;
-            //sayi1 ?? 30
+            sayi2 = 45;
+            //sayi1 ?? 50
 
-            int[] sayilar1 = new int[] { 10, 20, 30 };
-            int[] sayilar2 = new int[] { 100, 200, 300 };
+            int[] sayilar1 = new int[] { 100, 200, 300 };
+            int[] sayilar2 = new int[] { 1000, 2000, 3000 };
             sayilar1 = sayilar2;
             sayilar2[0] = 999;
             //sayilar1[0] 999
